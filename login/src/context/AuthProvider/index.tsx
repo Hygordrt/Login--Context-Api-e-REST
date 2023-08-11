5694943d1d0ade2ba4c62c553409ad880e1fa7e6
Hygor <Hygor@DESKTOP-55BA59J>
2023-08-11 19:27:33 -0300

    Contexto finalizado

--- login/src/context/AuthProvider/index.tsx
+++ login/src/context/AuthProvider/index.tsx
@@ -1,23 +1,35 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useEffect, useState} from "react";
 import { IAuthProvider, IContext, IUser } from "./types";
-import { LoginRequest } from "./util";
+import { LoginRequest, getUserLocalStorage, setUserLocalStorage } from "./util";
 
 export const AuthContext = createContext<IContext>({} as IContext)
 
 export const AuthProvider = ({ children }: IAuthProvider) => {
     const [user, setUser] = useState<IUser | null>()
 
+
+    useEffect(() => {
+        const user = getUserLocalStorage()
+        //Carregar o contexto se o user estiver logado
+        if (user) {
+            setUser(user);
+        }
+    }, [])
+    
     async function authenticate (email:string, password:string) {
         const response = await LoginRequest(email, password)
 
         const payload = {token: response.token, email}
 
+        //Salvo o stado do usuario em memoria
         setUser(payload);
-
+        //Salvo o stado do usuario no local storage
+        setUserLocalStorage(payload)
     }
 
     function logout () {
-        setUser(null)
+        setUser(null);
+        setUserLocalStorage(null);
     }
 
     return (
